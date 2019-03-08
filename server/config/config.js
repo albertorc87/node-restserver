8f74cd49f062aca081d337f8f649b419bd7af62a
Alberto Ramírez <alberto.r.caballero.87@gmail.com>
2019-03-08 22:32:41 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,19 @@
 //====================
 // Puerto
 //====================
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+// Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV !== 'dev') {
+    urlDB = 'mongodb+srv://katon:JPvqED6Kge3PiVap@cluster0-baofm.mongodb.net/cafe';
+} else {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,39 +1,21 @@
 require('./config/config');
 
-const express = require('express')
+const express = require('express');
+//"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="c:\data\db"
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(require('../routes/usuario'));
 
-// parse application/json
-app.use(bodyParser.json())
-
-app.get('/usuario', function(req, res) {
-    res.send('Get user')
-});
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({ body })
-    }
-
-});
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.send('Put user: ' + id);
-});
-app.delete('/usuario', function(req, res) {
-    res.send('Delete user')
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto: ', process.env.PORT);
 })
+
+//JPvqED6Kge3PiVap
